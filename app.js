cdf252bb4de0fa17781fc8a1d13525b215c50f70
nath-asha <omthasha@gmail.com>
2024-03-28 00:38:14 +0530

    v

--- app.js
+++ app.js
@@ -12,41 +12,6 @@ require('dotenv').config();
 app.use(express.json());
 
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-// Connect to MongoDB
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// Connection events
-const dbConnection = mongoose.connection;
-
-dbConnection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
-
-dbConnection.on('error', (err) => {
-  console.error('MongoDB connection error:', err.message);
-});
-
-dbConnection.on('disconnected', () => {
-  console.log('Disconnected from MongoDB');
-});
-
-// Close MongoDB connection when Node.js process terminates
-process.on('SIGINT', async () => {
-  try {
-    await mongoose.connection.close();
-    console.log('MongoDB connection closed due to application termination');
-    process.exit(0);
-  } catch (error) {
-    console.error('Error closing MongoDB connection:', error.message);
-    process.exit(1);
-  }
-});
-
 // Routes
 app.use('/auth', authRoutes);
 app.use('/food', foodRoutes);
--- config/database.js
+++ config/database.js
@@ -1,40 +1,40 @@
-// const mongoose = require('mongoose');
-// require('dotenv').config();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-// // MongoDB connection URI from environment variable
-// const MONGODB_URI = process.env.MONGODB_URI;
+// MongoDB connection URI from environment variable
+const MONGODB_URI = process.env.MONGODB_URI;
 
-// // Connect to MongoDB
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+// Connect to MongoDB
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// // Connection events
-// const dbConnection = mongoose.connection;
+// Connection events
+const dbConnection = mongoose.connection;
 
-// dbConnection.on('connected', () => {
-//   console.log('Connected to MongoDB');
-// });
+dbConnection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
 
-// dbConnection.on('error', (err) => {
-//   console.error('MongoDB connection error:', err.message);
-// });
+dbConnection.on('error', (err) => {
+  console.error('MongoDB connection error:', err.message);
+});
 
-// dbConnection.on('disconnected', () => {
-//   console.log('Disconnected from MongoDB');
-// });
+dbConnection.on('disconnected', () => {
+  console.log('Disconnected from MongoDB');
+});
 
-// // Close MongoDB connection when Node.js process terminates
-// process.on('SIGINT', async () => {
-//   try {
-//     await mongoose.connection.close();
-//     console.log('MongoDB connection closed due to application termination');
-//     process.exit(0);
-//   } catch (error) {
-//     console.error('Error closing MongoDB connection:', error.message);
-//     process.exit(1);
-//   }
-// });
+// Close MongoDB connection when Node.js process terminates
+process.on('SIGINT', async () => {
+  try {
+    await mongoose.connection.close();
+    console.log('MongoDB connection closed due to application termination');
+    process.exit(0);
+  } catch (error) {
+    console.error('Error closing MongoDB connection:', error.message);
+    process.exit(1);
+  }
+});
 
-// module.exports = dbConnection;
+module.exports = dbConnection;
